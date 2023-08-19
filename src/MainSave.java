d2622cca33b0e976337b2fa12a19c708ed309e8a
Homo-Faber-Fa <133135868+Homo-Faber-Fa@users.noreply.github.com>
2023-08-19 17:30:59 +0300

    Add files via upload

---
+++ src/MainSave.java
@@ -0,0 +1,84 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class MainSave {
+    public static void main(String[] args) {
+        List<GameProgress> listGameProgress = new ArrayList<>(); // сформирован объект массива содержащий переменные типа GameProgress
+        Collections.addAll(listGameProgress,                     // добавление объектов массива
+                new GameProgress(11, 12, 13, 1.4),
+                new GameProgress(21, 22, 23, 2.4),
+                new GameProgress(31, 32, 33, 3.4));
+                                                    // сформированы три объекта типа GameProgress
+
+        List<String> fileNameAPathFiles = saveGames("Games/savegames/", listGameProgress); // запуск saveGame  Запуск передает в метод путь папки и Массив GameProgress
+        //C:\Java\Java Core\HW 3 File\Games\savegames\
+
+        zipFiles("Games/savegames/", fileNameAPathFiles);           // запуск zipFiles   Запуск передает в метод путь папки и Имена файлов
+
+        deleteFiles(fileNameAPathFiles);
+    }
+
+    public static List<String> saveGames(String path, List<GameProgress> list) {
+
+        List<String> pathToZip = new ArrayList<>();  // сформирован объект массива содержащий переменные типа String
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+            GameProgress gameProgress = list.get(i);
+                stringBuilder.append(path).append("/save").append(i+1).append(".dat"); // Цикл формирования имени файла включая путь и расширение (отдельный оборот для каждого файла)
+
+            try (FileOutputStream fos = new FileOutputStream(String.valueOf(stringBuilder));
+                 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                oos.writeObject(gameProgress);
+            } catch (IOException e) { e.getMessage();}
+
+            pathToZip.add(String.valueOf(stringBuilder));
+
+            stringBuilder.setLength(0);
+        }
+        return pathToZip;
+    }
+
+    public static void zipFiles(String pathToDirectory, List<String> pathToFiles) {
+
+        StringBuilder sb = new StringBuilder().append(pathToDirectory).append("/.zip");
+        try (ZipOutputStream arhops = new ZipOutputStream(new FileOutputStream(String.valueOf(sb)))) {
+            pathToFiles.stream().forEach(x -> {
+                try {
+                    FileInputStream fips = new FileInputStream(x);
+                    sb.setLength(0);
+                    sb.append(x).replace(0, 19, "zip_");
+                    ZipEntry entry = new ZipEntry(String.valueOf(sb));
+                    arhops.putNextEntry(entry);
+                    byte[] buffer = new byte[fips.available()];
+                    fips.read(buffer);
+                    arhops.write(buffer);
+                    arhops.closeEntry();
+                    fips.close();
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+            });
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+    }
+
+    private static void deleteFiles(List<String> path) {
+        int count = 0;
+        for (int i = 0; i < path.size(); i++) {
+            File file = new File(path.get(i));
+            if (file.delete()) {
+                count = count + 1;
+            }
+        }
+        if (count == 3) { System.out.println("файлы удалены");}
+    }
+}
--- src/MainSetUp.java
+++ src/MainSetUp.java
@@ -1,1 +1,60 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class MainSetUp {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        //HW JC №3 Задача 1: Установка
+
+        File dir = new File("Games");  if (dir.mkdir())  sb.append(" Games каталог создан \n");
+        File dir1 = new File("Games/src");  if (dir1.mkdir()) sb.append(" Games/src каталог создан \n");
+        File dir2 = new File("Games/res");  if (dir2.mkdir()) sb.append(" Games/res каталог создан \n");
+        File dir3 = new File("Games/savegames");  if (dir3.mkdir()) sb.append(" Games/savegames каталог создан \n");
+        File dir4 = new File("Games/temp");  if (dir4.mkdir()) sb.append(" Games/temp каталог создан \n");
+        File dir5 = new File("Games/src/main");  if (dir5.mkdir()) sb.append(" Games/src/main каталог создан \n");
+        File dir6 = new File("Games/src/test");  if (dir6.mkdir()) sb.append(" Games/src/test каталог создан \n");
+
+
+
+
+
+        File myFile0 = new File("Games/src/main/Main.java");
+        try { if (myFile0.createNewFile())
+            sb.append(" Main.java файл был создан \n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());}
+
+        File myFile1 = new File("Games/src/main/Utils.java");
+        try { if (myFile1.createNewFile())
+            sb.append(" Utils.java файл был создан \n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());}
+
+        File myFile2 = new File("Games/temp/temp.txt");
+        try { if (myFile2.createNewFile())
+            sb.append(" temp.txt файл был создан \n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());}
+
+
+        System.out.println(sb.toString());
+
+
+        String text = sb.toString();
+        byte[] buffer = text.getBytes();
+// создаем выходной байтовый поток
+// и передаем его в выходной буферизированный поток
+        try (FileOutputStream out = new FileOutputStream("Games\\temp\\temp.txt");
+             BufferedOutputStream bos = new BufferedOutputStream(out)) {
+            // производим запись от 0 до последнего байта из массива
+            bos.write(buffer, 0, buffer.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
+    }
+}
